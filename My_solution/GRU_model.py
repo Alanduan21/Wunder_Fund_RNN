db813ea8392c1f8abd3275b5fa840e38691e4184
Duan Yihe <dtc20904newgame@gmail.com>
2025-11-27 07:37:26 +0800

    changing validation parameter to fit into new training

--- My_solution/GRU_model.py
+++ My_solution/GRU_model.py
@@ -6,13 +6,13 @@ from torch.utils.data import TensorDataset, DataLoader
 # Model class
 
 class GRUModel(nn.Module):
-    def __init__(self, input_size, hidden_size=64, num_layers=2):
+    def __init__(self, input_size, hidden_size=128, num_layers=3, dropout=0.2):
         super().__init__()
         self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=num_layers, batch_first=True)
+                          num_layers=num_layers, batch_first=True, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(hidden_size, 1)
 
     def forward(self, x):
         out, _ = self.gru(x)
         return self.fc(out[:, -1, :])
-
